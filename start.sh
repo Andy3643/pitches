1827fda448686a271032aab9576f5e92f721d975
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-13 00:45:51 +0300

    display all pitches

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export SECRET_KEY= "secretkey1"
 export APP_SETTINGS="config.DevelopmentConfig"
 export DATABASE_URL='sqlite:///db.sqlite'
+export MAIL_USERNAME='pitcheshubapp@gmail.com'
+export MAIL_PASSWORD='Get#yourinfo'
 python3 manage.py server
