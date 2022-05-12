3d7a26e13a9af66cafd3304c4df92ec3f7b500df
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-12 23:44:02 +0300

    add logout authentication route

--- app/auth/views.py
+++ app/auth/views.py
@@ -38,3 +38,10 @@ def login():
 
 
     return render_template('login.html',login_form = form)
+
+@auth.route('/logout')
+@login_required
+def signout():
+    logout_user()
+    flash('You have been successfully logged out')
+    return redirect(url_for("main.index"))
