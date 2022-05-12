e3aa8e9c34553387c62ae608816aee05754feb22
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-12 22:59:45 +0300

    Add redirect routes/ view database

--- manage.py
+++ manage.py
@@ -27,4 +27,5 @@ def test():
 
 
 if __name__== "__main__":
+    db.create_all()
     manager.run()
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export SECRET_KEY= "secretkey1"
 export APP_SETTINGS="config.DevelopmentConfig"
-export DATABASE_URL=''
+export DATABASE_URL='sqlite:///db.sqlite'
 python3 manage.py server
